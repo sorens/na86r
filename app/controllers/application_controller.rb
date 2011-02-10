62856bbffd37a6e40fffdea69b40b3447af76cd5
Steve Orens <steve@orens.com>
2011-02-10 08:45:54 -0800

    Adding Devise for User auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+  
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery
 end
