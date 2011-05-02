fc09cbf5acde967d98ba7cfda1d830eaea743683
Steve Orens <steve@orens.com>
2011-05-01 22:05:57 -0700

    first-pass at action reports. not ideal.

--- lib/naval_game_one/action_report.rb
+++ lib/naval_game_one/action_report.rb
@@ -2,68 +2,19 @@ require 'string'
 
 module NavalGameOne
   class ActionReport
-    attr_accessor :target_key, :action_key, :source_key, :target_origin_key,
-    :source_origin_key, :actor_key, :actor_origin_key, :method_key,
-    :method_type_key
+    attr_accessor :target, 
+    :action, 
+    :source, 
+    :target_origin,
+    :source_origin, 
+    :actor, 
+    :actor_origin, 
+    :method,
+    :method_type,
+    :message
 
     def to_s
-      #"CVN NIMITZ HAS BEEN SUNK BY AS-6 MISSILES LAUNCHED FROM RAID #123."
-      "#{target}#{action}#{method_type}#{method}FROM#{source}."
+      self.message
     end
-
-    private
-    def target
-      case @target_key
-      when "cvn68"
-        "cvn-nimitz ".upcase
-      else
-        ""
-      end
-    end
-
-    def action
-      case @action_key
-      when "sunk"
-        "has been sunk by ".upcase
-      else
-        ""
-      end
-    end
-
-    def method_type
-      case @method_type_key
-      when "as6"
-        "as-6 ".upcase
-      else
-        ""
-      end
-    end
-
-    def method
-      case @method_key
-      when "missile"
-        "missiles launched ".upcase
-      when "torpedo"
-        "torpedos launched ".upcase
-      else
-        ""
-      end
-    end
-
-    def source
-      unless @source_key.nil?
-        if @source_key.starts_with?( "raid" )
-          " " + @source_key.gsub( /_/, " #" ).upcase
-        else
-          case @source_key
-          when "ssn126"
-            " ssn-swiftsure".upcase
-          else
-            ""
-          end
-        end
-      end
-    end
-
   end
 end
--- spec/naval_game_one/action_report_spec.rb
+++ spec/naval_game_one/action_report_spec.rb
@@ -1,44 +1,62 @@
 require 'spec_helper'
 
+# I've changed how I want to implement this...
 module NavalGameOne
 	describe ActionReport do
-		# let( :options )					{ options = { :name => 'Scapa Flow', 
-		# 												:max_allowed_combat_aircraft => 100 }  }
-		# let( :strike_aircraft )	{ ["F15", 10] }
-		# let( :target )					{ double( 'target', :id => 11, :name => "Task Force #{:id}" ) }
-		# 
-		# before( :each ) do
-		# 	@airbase = Airbase.new( options )
-		# 	@airbase.add_aircraft( "F14", 20 )
-		# 	@airbase.add_aircraft( "F15", 20 )
-		# end
-		
 		# CVN-NIMITZ HAS BEEN SUNK BY AN AS-6 MISSILE LAUNCHED FROM RAID #123.
 		# AP-BASKUNCHAK HAS BEEN SUNK BY A TORPEDO FROM SSN-SWIFTSURE.
 		# AEW AIRCRAFT FROM SCAPA FLOW DETECTS MISSILES APPROACHING TF #20
 		
 		it "should report a a ship sunk by a missile launched from a raid" do
 			report = ActionReport.new
-			report.target_key = "cvn68"
-			report.action_key = "sunk"
-			report.source_key = "raid_123"
-			report.target_origin_key = nil
-			report.source_origin_key = "iceland"
-			report.actor_key = nil
-			report.actor_origin_key = nil
-			report.method_key = "missile"
-			report.method_type_key = "as6"
+			report.target = "cvn68"
+			report.action = "sunk"
+			report.source = "raid_123"
+			report.target_origin = nil
+			report.source_origin = "iceland"
+			report.actor = nil
+			report.actor_origin = nil
+			report.method = "missile"
+			report.method_type = "as6"
+			report.message = "CVN-NIMITZ HAS BEEN SUNK BY AS-6 MISSILES LAUNCHED FROM RAID #123."
 			report.to_s.should == "CVN-NIMITZ HAS BEEN SUNK BY AS-6 MISSILES LAUNCHED FROM RAID #123."
 		end
 		
 		it "should report a ship sunk by a torpedo launched from a submarine" do
 		  report = ActionReport.new
-		  report.target_key = "cvn68"
-		  report.action_key = "sunk"
-		  report.source_key = "ssn126"
-		  report.method_key = "torpedo"
+		  report.target = "cvn68"
+		  report.action = "sunk"
+		  report.source = "ssn126"
+		  report.method = "torpedo"
+			report.message = "CVN-NIMITZ HAS BEEN SUNK BY TORPEDOS LAUNCHED FROM SSN-SWIFTSURE."
 		  report.to_s.should == "CVN-NIMITZ HAS BEEN SUNK BY TORPEDOS LAUNCHED FROM SSN-SWIFTSURE."
 	  end
+	  
+	  it "should report a ship hit by a missile" do
+	    report = ActionReport.new
+	    report.target = "cvn68"
+	    report.action = "hit"
+	    report.source = "raid_123"
+	    report.method = "missile"
+	    report.method_type = "as6"
+			report.message = "CVN-NIMITZ HAS BEEN HIT BY AS-6 MISSILES LAUNCHED FROM RAID #123."
+	    report.to_s.should == "CVN-NIMITZ HAS BEEN HIT BY AS-6 MISSILES LAUNCHED FROM RAID #123."
+    end
+    
+    it "should report inbound missiles detected" do
+      report = ActionReport.new
+      report.target = "tf_11"
+      report.action = "detected"
+      report.source = "raid_123"
+      report.target_origin = nil
+      report.source_origin = "iceland"
+      report.actor = "aew"
+      report.actor_origin = "scapa_flow"
+      report.method = "aircraft"
+      report.method_type = nil
+			report.message = "AEW AIRCRAFT FROM SCAPA FLOW DETECTS MISSILES LAUNCHED FROM RAID #123 APPROACHING TF #11."
+      report.to_s.should == "AEW AIRCRAFT FROM SCAPA FLOW DETECTS MISSILES LAUNCHED FROM RAID #123 APPROACHING TF #11."
+    end
 		
 	end
 end 
