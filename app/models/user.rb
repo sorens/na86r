57ed52d798a5eefdd9599fe40d956849ebfc1c97
Steve Orens <steve@orens.com>
2011-08-22 01:01:56 -0700

    used the wrong filter to generate an auth token

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
          :validatable,
          :token_authenticatable
 
-  before_save :reset_authentication_token
+  before_save :ensure_authentication_token
   
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
