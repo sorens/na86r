0b835c445e57bab15ef041ad08f52e84f1a8a9b3
Steve Orens <steve@orens.com>
2011-09-21 20:50:56 -0700

    placeholder seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# Bases
+
+# Ships
+
+# Aircraft?
+
+# Task Forces
