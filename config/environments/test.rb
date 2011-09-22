387e972ac822f2734dc38fea5b0759725d8193c3
Steve Orens <steve@orens.com>
2011-09-21 20:52:35 -0700

    use simple_logger for test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+require File.expand_path( '../../simple_logger.rb', __FILE__ )
+
 NavalConflict::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -32,4 +34,19 @@ NavalConflict::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
+
+  # turn off color logging messages
+  config.colorize_logging = false
+  
+  # use our simple logger class so we can see a date time stamp for each entry
+  # path = File.expand_path( "#{Rails.root}/log/#{Rails.env}.log", __FILE__ )
+  path = File.expand_path( "~/logs/#{Rails.application.class.parent_name}_#{Rails.env}.log", __FILE__ )
+  logfile = File.open( path, 'a' )
+  logfile.sync = true
+  Rails.logger = SimpleLogger.new( logfile )
+  Rails.logger.debug "Configured to use SimpleLogger"
 end
