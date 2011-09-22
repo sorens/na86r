f4f149bc571b459178a1840eb0ecb1ea490cdd4b
Steve Orens <steve@orens.com>
2011-09-21 20:52:06 -0700

    turned off rjs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ NavalConflict::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_view.debug_rjs             = true
+  config.action_view.debug_rjs             = false
   config.action_controller.perform_caching = false   #set to true and restart server to enable caching
   config.action_controller.page_cache_directory = "#{Rails.public_path}/cache/"
 
