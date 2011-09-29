294f70b50ea1811252b3738b2cc33bbaea3d3a0e
Steve Orens <steve@orens.com>
2011-09-28 19:46:16 -0700

    add a navigation link to the admin dashboard

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -35,4 +35,9 @@ ActiveAdmin::Dashboards.build do
   #
   # Will render the "Recent Users" then the "Recent Posts" sections on the dashboard.
 
+  section "Navigation" do
+    ul do
+      li link_to "Naval Conflict", welcome_index_path
+    end
+  end
 end
