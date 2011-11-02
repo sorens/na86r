8d5788497623982483cc0effbf4fd2cfc45c2901
Steve Orens <steve@orens.com>
2011-11-02 09:48:24 -0700

    maybe the mailer url needs to be heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ NavalConflict::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'navalconflict.com' }
+  config.action_mailer.default_url_options = { :host => 'navalconflict.heroku.com' }
 
   # Enable threaded mode
   # config.threadsafe!
