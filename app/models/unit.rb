6d413e34009212635041637d298d377a56ecc2af
Steve Orens <steve@orens.com>
2011-11-02 00:42:04 -0700

    fixed Parsed module. Modified the Unit model to use the Parsed module to store the unit data

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,6 +1,5 @@
 class Unit < ActiveRecord::Base
-  # include Parsed
-  @parsed_data
+  include Parsed
   
   MAIN_GUN        = "main_gun"
   ANTI_AIRCRAFT   = "anti_aircraft"
@@ -14,12 +13,14 @@ class Unit < ActiveRecord::Base
   STATUS          = "status"
   MAX_DAMAGE      = "max_damage"
   
-  # status
+  # unit status
   STATUS_AVAILABLE    = "available"
   STATUS_SUNK         = "sunk"
   STATUS_SCUTTLED     = "scuttled"
   STATUS_UNKOWN       = "unknown"
   STATUS_IN_PIPELINE  = "in_pipeline"
+  STATUS_DESTROYED    = "destroyed"
+  STATUS_CRASHED      = "crashed"
   
   #
   def max_damage
@@ -28,70 +29,96 @@ class Unit < ActiveRecord::Base
   
   #
   def apply_damage( value )
-    Rails.logger.debug "applying damage [#{value}]"
+    # Rails.logger.debug "applying damage [#{value}]"
     self.current_damage= value
-    Rails.logger.debug "current_damage is now [#{current_damage}]"
-    self.status= STATUS_SUNK if ( current_damage >= max_damage )
-    Rails.logger.debug "status is now [#{status}]"
+    # Rails.logger.debug "current_damage is now [#{current_damage}]"
+    self.status= STATUS_SUNK if ( self.current_damage >= self.max_damage )
+    # Rails.logger.debug "status is now [#{status}]"
   end
   
   # be sure to load our JSON data into
   # into a hash that we can easily utilize
   def initialize( options = {} )
-    @fields = []
+
+    # initialize our module
+    parsed_initialize
+    # add the attributes that we're expecting
+    add_attribute MAIN_GUN
+    add_attribute ANTI_AIRCRAFT
+    add_attribute MISSILE_DEFENSE
+    add_attribute MAX_SPEED
+    add_attribute CARGO_CAPACITY
+    add_attribute DEFENSE_FACTOR
+    add_attribute TASK_FORCE
+    add_attribute ARRIVAL_DAYS
+    add_attribute CURRENT_DAMAGE
+    add_attribute STATUS
+    add_attribute MAX_DAMAGE
+    
+    # call our super
     super( options )
+    # load our parsed data
     load_parsed_data
+    # if this is a first time setup, make sure we set an initial
+    # current_damage of 0
     if @parsed_data and ! @parsed_data.has_key?( CURRENT_DAMAGE )
-      @parsed_data[CURRENT_DAMAGE] = 0
+      self.current_damage = 0
       save
     end
   end
   
-  def method_missing( name, *args, &block )
-    setter = false
-    method_name = name.to_s
-    setter = true if method_name and method_name[-1,1] == "="
-    method_name = method_name[0,method_name.length-1] if setter
-    if @parsed_data and @parsed_data.has_key?( method_name )
-      if setter
-        # a store function
-        store_data( method_name, args[0] )
-      else
-        # a fetch function
-        fetch_data( method_name )
-      end
-    else
-      Rails.logger.debug "tried to handle [#{method_name}] with args: %p" % [args] unless args.empty?
-      super
-    end
-  end
+  # def method_missing( name, *args, &block )
+  #   setter = false
+  #   method_name = name.to_s
+  #   setter = true if method_name and method_name[-1,1] == "="
+  #   method_name = method_name[0,method_name.length-1] if setter
+  #   if @parsed_data and @parsed_data.has_key?( method_name )
+  #     if setter
+  #       # a store function
+  #       store_data( method_name, args[0] )
+  #     else
+  #       # a fetch function
+  #       fetch_data( method_name )
+  #     end
+  #   else
+  #     Rails.logger.debug "tried to handle [#{method_name}] with args: %p" % [args] unless args.empty?
+  #     super
+  #   end
+  # end
   
   private
   
-  def fetch_data( key )
-    if @parsed_data and @parsed_data.has_key?( key )
-      @parsed_data[key]
-    else
-      nil
-    end
-  end
+  # def fetch_data( key )
+  #   if @parsed_data and @parsed_data.has_key?( key )
+  #     @parsed_data[key]
+  #   else
+  #     nil
+  #   end
+  # end
+  # 
+  # def store_data( key, value )
+  #   if @parsed_data
+  #     @parsed_data[key] = value
+  #     save
+  #   end
+  # end
   
-  def store_data( key, value )
-    if @parsed_data
-      @parsed_data[key] = value
-      save
-    end
-  end
-  
+  # override our save function to make sure that we
+  # have a chance to turn our hash back into JSON data
+  # for storage
   def save
-    data = @parsed_data.to_json
+    self.data = @parsed_data.to_json
     super
   end
   
+  # grab the string data in self.data and parse it as
+  # JSON data. if valid, load it into our @parsed_data
+  # attribute for ready use
   def load_parsed_data
     if @parsed_data.nil? and ! self.data.nil?
       begin
         @parsed_data = JSON.parse( self.data )
+        save
       rescue JSON::ParserError
         @parsed_data = nil
       end
--- lib/parsed.rb
+++ lib/parsed.rb
@@ -1,33 +1,89 @@
+# defines methods and attributes used to manage a string of data
+# that is turned into a Hash at load time. This facilitates having
+# to expand our data structures without having to add columns to our
+# database tables
 module Parsed
 
-  def valid_attribute?( name )
-    # return false if @fields.nil?
-    @fields.include?( name )
+  # attributes that this module is expecting to have provided
+  # by the including class
+  attr :parsed_fields
+  attr :parsed_data
+  
+  # initialize our module
+  def parsed_initialize
+    @parsed_fields = []
   end
   
+  # validate that this key is could exist in our
+  # @parsed_data hash
+  def valid_attribute?( name )
+    # Rails.logger.debug "validating attribute [#{name}]"
+    # Rails.logger.debug "fields was nil" if @parsed_fields.nil?
+    return false if @parsed_fields.nil?
+    # Rails.logger.info "fields includes [#{name}]" if @parsed_fields.include?( name )
+    return @parsed_fields.include?( name )
+  end
+  
+  # store which attributes we're expecting so we
+  # can pre-determine if we should look up a key
+  # in our @parsed_data
   def add_attribute( name )
-    unless @fields.include?( name )
-      @fields << name
+    unless @parsed_fields.include?( name )
+      @parsed_fields << name
     end
   end
   
-  def method_missing( name_symbol, *params )
+  # fetch the value for this key from our @parsed_data
+  # hash
+  def fetch_data( key )
+    # Rails.logger.debug "[#{@parsed_data.inspect}]"
+    if @parsed_data and @parsed_data.has_key?( key )
+      return @parsed_data[key]
+    else
+      # Rails.logger.debug "no value for [#{key}]"
+      nil
+    end
+  end
+  
+  # store this key/value pair in our @parsed_data
+  # hash for later serialization
+  def store_data( key, value )
+    if @parsed_data
+      # Rails.logger.info "parsed_data is non-nil"
+      @parsed_data[key] = value
+      save
+      # Rails.logger.debug "saved [#{key}] with [#{value}]"
+    # else
+    #   Rails.logger.debug "parsed_data is nil"
+    end
+  end
+  
+  # use method_missing to intercept would-be getters/setter
+  # calls via straight-forward syntax such as:
+  #
+  # self.a = "blah"
+  # puts "#{self.a}"
+  #
+  def method_missing( name_symbol, *params, &block )
     name = name_symbol.to_s
-    Rails.logger.info "method_missing [#{name}]"
+    # Rails.logger.debug "method_missing [#{name}]"
     return if name =~ /^A-Z/
     if name.to_s[-1] == ( '='[0] )    # we have a setter
+      # Rails.logger.debug "setter!"
       is_setter = true
       name.sub!( /=$/, '' )
     end
     
     if valid_attribute?( name )
       if is_setter
-        instance_variable_set( "@parsed_data[#{name}]", *params )
+        # Rails.logger.debug "storing data [#{name}] [#{params}]"
+        store_data( name, *params )
       else
-        instance_variable_get( "@parsed_data[#{name}]", *params )
+        # Rails.logger.debug "fetching data [#{name}]"
+        fetch_data( name )
       end
-    # else
-    #   super( name_symbol, *params )
+    else
+      super( name_symbol, *params, &block )
     end
   end
 end
