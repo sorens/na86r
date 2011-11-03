9777e2311ac14d8aeeafa19f9d4a006d507f907f
Steve Orens <steve@orens.com>
2011-11-02 20:25:31 -0700

    simple test for the notice, alert and error banners

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,14 @@
 class WelcomeController < ApplicationController
+  
+  def index
+    if params[:alert]
+      flash.now[:alert] = params[:alert]
+    end
+    if params[:notice]
+      flash.now[:notice] = params[:notice]
+    end
+    if params[:error]
+      flash.now[:error] = params[:error]
+    end
+  end
 end
