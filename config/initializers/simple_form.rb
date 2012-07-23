146044787fae2c52699d23cda8c9dfaccbbb4b20
Steve Orens <steve@orens.com>
2012-07-23 00:25:42 -0700

    updating

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -1,6 +1,14 @@
 SimpleForm.setup do |config|
-  config.hint_class = "hint input"
-  config.error_class = 'error input'
-  config.wrapper_class = :clearfix
-  config.wrapper_error_class = :error
+  config.wrappers :tag => :p, :class => :input,
+                     :error_class => :fieldWithErrors do |b|
+      b.use :html5
+      b.use :maxlength
+      b.use :placeholder
+      b.use :readonly
+      b.use :pattern
+
+      b.use :label_input
+      b.use :hint,  :wrap_with => { :tag => :span, :class => :inputHint }
+      b.use :error, :wrap_with => { :tag => :span, :class => :inputError }
+    end
 end
