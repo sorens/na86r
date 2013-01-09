55cada9dfc81f749b2f1f58b1e2e05a0593fe6e4
Steve Orens <steve@orens.com>
2013-01-08 22:52:52 -0800

    no-op this migration

--- db/migrate/20110822062715_add_token_authentication_field_to_user.rb
+++ db/migrate/20110822062715_add_token_authentication_field_to_user.rb
@@ -1,14 +1,14 @@
 class AddTokenAuthenticationFieldToUser < ActiveRecord::Migration
   def self.up
-    change_table :users do |t|
-      t.token_authenticatable
-    end
+    # change_table :users do |t|
+    #   t.token_authenticatable
+    # end
     
-    add_index :users, :authentication_token, :unique => true
+    # add_index :users, :authentication_token, :unique => true
   end
 
   def self.down
-    remove_column :users, :authentication_token
-    remove_index  :users, :authentication_token
+    # remove_column :users, :authentication_token
+    # remove_index  :users, :authentication_token
   end
 end
